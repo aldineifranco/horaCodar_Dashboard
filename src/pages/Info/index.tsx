111e7013bb344971f64423b26807e101cf7d7b35
Aldinei <aldinei.franco@unimedprudente.com.br>
2023-11-03 16:55:34 -0300

    form

--- src/pages/Info/index.tsx
+++ src/pages/Info/index.tsx
@@ -1,9 +1,58 @@
-import { Container } from "./styles";
+import { Container, Formulario } from "./styles";
 
 export function Info() {
+  const diasSemana = [
+    {
+      id: 1,
+      dia: "Segunda",
+    },
+    {
+      id: 2,
+      dia: "Terça",
+    },
+    {
+      id: 3,
+      dia: "Quarta",
+    },
+    {
+      id: 4,
+      dia: "Quinta",
+    },
+    {
+      id: 5,
+      dia: "Sexta",
+    },
+    {
+      id: 6,
+      dia: "Sábado",
+    },
+    {
+      id: 7,
+      dia: "Domingo",
+    },
+  ]
+
   return (
     <Container>
-      <h1>Info</h1>
+      <div>
+        <h1>Dias da Semana</h1>
+
+        <Formulario>
+          {
+            diasSemana.map((dia) => {
+              return (
+                <div>
+                  <label htmlFor="" key={dia.id}>
+                    {dia.dia}:
+                  </label>
+                  <input type="text" />
+                </div>
+             
+              )
+            })
+          }
+        </Formulario>
+      </div>
     </Container>
   )
 }
--- src/pages/Info/styles.ts
+++ src/pages/Info/styles.ts
@@ -2,4 +2,23 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   grid-area: content;
+
+  margin: 20px;
+`;
+
+export const Formulario = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  input {
+    display: flex;
+    padding: .75rem;
+    background-color: transparent;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    color: #fff;
+  }
+
+
 `;
